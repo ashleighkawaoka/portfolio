ae898074108b6dc9cde242fbae2b9ecaf1c716a4
Ashleigh Kawaoka <your-email@example.comclear>
2026-02-20 16:25:40 -0800

    favicon-2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,10 +8,15 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://use.typekit.net/qgr7wto.css'
-        }
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'stylesheet', href: 'https://use.typekit.net/qgr7wto.css' }
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-title', content: 'Ashleigh Kawaoka' }
       ]
     }
   }
